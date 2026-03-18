a554f61c661db61143b7c1877468251030cb5dc7
pabeyawardhana <Pamoda_Abeyawardhana@ajg.com>
2026-03-18 11:31:25 +0530

    Refactor Terraform configuration for improved readability and add .terraform.lock.hcl

--- main.tf
+++ main.tf
@@ -6,16 +6,16 @@ terraform {
       version = ">= 3.0"
     }
   }
-  
+
 }
 
 
 resource "azurerm_storage_account" "securestorage" {
- 
+
   resource_group_name      = var.resource_group_name
-  location                = var.location
-  name                    = var.storage_account_name
-  account_tier = var.account_tier
+  location                 = var.location
+  name                     = var.storage_account_name
+  account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
-  
+
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output storage_account_id {
-  value       = azurerm_storage_account.securestorage.id
-  
+output "storage_account_id" {
+  value = azurerm_storage_account.securestorage.id
+
 }
